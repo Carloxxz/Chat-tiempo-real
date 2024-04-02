fbb68ca1daab6cd6b3fa776f3b101f4fbb8e19e5
Carloxxz <carloxmz@gmail.com>
2024-04-01 22:33:05 -0600

    -Implementa base de datos con turso -persistencia en mensajes

--- server/index.js
+++ server/index.js
@@ -1,31 +1,77 @@
 import express from 'express'
 import logger from 'morgan'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
+
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+
+dotenv.config()
 
 const port = process.env.PORT ?? 3000
 
 const app = express()
 const server = createServer(app)
-const io = new Server(server)
-app.use(logger('dev'))
+const io = new Server(server, {
+    connectionStateRecovery: {}
+})
 
-io.on('connection', (socket) => {
+const db = createClient({
+    url: "libsql://intense-the-shadow-carloxxz.turso.io",
+    authToken: process.env.DB_TOKEN
+})
+
+await db.execute(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        content TEXT
+    )
+`)
+
+
+io.on('connection', async (socket) => {
     console.log('a user has connected!')
 
     socket.on('disconnect', () => {
         console.log('an user has disconnected')
     })
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg)
+    socket.on('chat message', async (msg) => {
+        let result
+        try {
+            result = await db.execute({
+                sql: `INSERT INTO messages (content) VALUES (:content)`,
+                args: { content: msg }
+            })
+        } catch (e) {
+            console.error(e)
+            return
+        }
+        io.emit('chat message', msg, result.lastInsertRowid.toString())
     })
+
+    if (!socket.recovered) {
+        try {
+            const results = await db.execute({
+                sql: 'SELECT id, content FROM messages WHERE id > ?',
+                args: [socket.handshake.auth.serverOffSet ?? 0]
+            })
+
+            results.rows.forEach(row =>
+                socket.emit('chat message', row.content, row.id.toString())
+            )
+        } catch (e) {
+            console.error(e)
+        }
+    }
 })
+
+app.use(logger('dev'))
 
 app.get("/", (req, res) => {
     res.sendFile(process.cwd() + '/client/index.html')
 })
 
 server.listen(port, () => {
-    console.log( `Server running on port ${port}`)
+    console.log(`Server running on port ${port}`)
 })
